1efe5f8a74ce1e014a8f6a8cd80aba7694b41e22
ukashasohail <ukasha909@outlook.com>
2021-01-27 00:50:40 +0500

    :zap:

--- hello.go
+++ hello.go
@@ -8,4 +8,20 @@ import (
 
 func main() {
 	fmt.Println(quote.Hello())
+
+	var returned int = max(5, 10)
+
+	fmt.Println(returned)
+}
+
+func max(num1, num2 int) int {
+
+	var result int
+
+	if num1 > num2 {
+		result = num1
+	} else {
+		result = num2
+	}
+	return result
 }
